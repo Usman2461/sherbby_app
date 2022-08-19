84b3fa2448918de84cdea25063017c01c4ab6471
usman <usmanarshad5051@outlook.com>
2022-08-20 02:14:26 +0500

    completed UI empty

--- lib/widgets/homePage.dart
+++ lib/widgets/homePage.dart
@@ -25,90 +25,90 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var orientation = MediaQuery.of(context).orientation;
     return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 10,
-          ),
-          CarouselSlider.builder(
-            options: CarouselOptions(
-                height: 200,
-                enlargeCenterPage: true,
-                viewportFraction: 1.0,
-                autoPlay: true,
-                onPageChanged: (index, reason) =>
-                    setState(() => activeIndex = index)),
-            itemCount: sliderimage.length,
-            itemBuilder: (context, index, realIndex) {
-              final imageslider = sliderimage[index];
-              return buildImage(imageslider, index);
-            },
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          buildIndicator(),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Tous les prodults disponibles",
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w500),
-                ),
-                Icon(Icons.arrow_forward_ios),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 0),
-            child: GridView(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: orientation == Orientation.landscape ? 4 : 2,
-                  mainAxisSpacing: 8,
-                  childAspectRatio: 20 / 28,
-                  crossAxisSpacing: 5),
-              children: [
-                GridWidget(
-                  urltext: 'Search Vehicle',
-                  ontap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>PostScreen()));
-                  },
-                  urlimage: 'assets/images/1.png',
-                ),
-                GridWidget(
-                  urltext: 'Add Request',
-                  ontap: () {
-                    Navigator.pushNamed(context, "/vtypeuser");
-                  },
-                  urlimage: 'assets/images/4.png',
-                ),
-                GridWidget(
-                  urltext: 'View Request',
-                  ontap: () {
-                    Navigator.pushNamed(context, "/requestScreenUser");
-                  },
-                  urlimage: 'assets/images/4.png',
-                ),
-                GridWidget(
-                  urltext: 'Settings',
-                  ontap: () {},
-                  urlimage: 'assets/images/4.png',
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+      // child: Column(
+      //   children: [
+      //     SizedBox(
+      //       height: 10,
+      //     ),
+      //     CarouselSlider.builder(
+      //       options: CarouselOptions(
+      //           height: 200,
+      //           enlargeCenterPage: true,
+      //           viewportFraction: 1.0,
+      //           autoPlay: true,
+      //           onPageChanged: (index, reason) =>
+      //               setState(() => activeIndex = index)),
+      //       itemCount: sliderimage.length,
+      //       itemBuilder: (context, index, realIndex) {
+      //         final imageslider = sliderimage[index];
+      //         return buildImage(imageslider, index);
+      //       },
+      //     ),
+      //     SizedBox(
+      //       height: 5,
+      //     ),
+      //     buildIndicator(),
+      //     SizedBox(
+      //       height: 5,
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.all(10.0),
+      //       child: Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //         children: [
+      //           Text(
+      //             "Tous les prodults disponibles",
+      //             style: TextStyle(
+      //                 fontSize: 18,
+      //                 color: Colors.grey,
+      //                 fontWeight: FontWeight.w500),
+      //           ),
+      //           Icon(Icons.arrow_forward_ios),
+      //         ],
+      //       ),
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 0),
+      //       child: GridView(
+      //         shrinkWrap: true,
+      //         physics: NeverScrollableScrollPhysics(),
+      //         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: orientation == Orientation.landscape ? 4 : 2,
+      //             mainAxisSpacing: 8,
+      //             childAspectRatio: 20 / 28,
+      //             crossAxisSpacing: 5),
+      //         children: [
+      //           GridWidget(
+      //             urltext: 'Search Vehicle',
+      //             ontap: () {
+      //               Navigator.push(context, MaterialPageRoute(builder: (context)=>PostScreen()));
+      //             },
+      //             urlimage: 'assets/images/1.png',
+      //           ),
+      //           GridWidget(
+      //             urltext: 'Add Request',
+      //             ontap: () {
+      //               Navigator.pushNamed(context, "/vtypeuser");
+      //             },
+      //             urlimage: 'assets/images/4.png',
+      //           ),
+      //           GridWidget(
+      //             urltext: 'View Request',
+      //             ontap: () {
+      //               Navigator.pushNamed(context, "/requestScreenUser");
+      //             },
+      //             urlimage: 'assets/images/4.png',
+      //           ),
+      //           GridWidget(
+      //             urltext: 'Settings',
+      //             ontap: () {},
+      //             urlimage: 'assets/images/4.png',
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 
